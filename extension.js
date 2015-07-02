9c9d8a7a8a1b0de8ed237f0259cec629693c9fc0
Jeff Minelli <jgm106@psu.edu>
2015-07-02 12:28:28 -0400

    trying out setting autoSkip

--- extension.js
+++ extension.js
@@ -512,6 +512,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
+        autoSkip: true,
         botName: "CletusBot",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
