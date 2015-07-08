c75d889fcaad687e94f7b67e570b47fa3cc5ad43
Jeff Minelli <jgm106@psu.edu>
2015-07-08 15:03:50 -0400

    drool old man -- stupid timer

--- extension.js
+++ extension.js
@@ -217,10 +217,10 @@
         if (!bot.commands.executable(this.rank, chat)) return void (0);
         else {
           API.sendChat(":tophat:");
-          API.sendChat(":eyes:");
-          API.sendChat(":nose:");
-          API.sendChat(":tongue:");
-          API.sendChat(":droplet:");
+          setTimeout(function(){API.sendChat(":eyes:")}, 1 * 250);
+          setTimeout(function(){API.sendChat(":nose:")}, 1 * 250);
+          setTimeout(function(){API.sendChat(":tongue:")}, 1 * 250);
+          setTimeout(function(){API.sendChat(":droplet:")}, 1 * 250);
         }
       }
     }; // drool||dom||DOM
