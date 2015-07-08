610fd1b0cd1b65474178bf1ef64e9f7f0918b5f7
Jeff Minelli <jgm106@psu.edu>
2015-07-08 12:29:37 -0400

    added a userscript for origem-woot plugin

---
+++ origem-woot.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name           origem-woot
+// @namespace      net.cletus
+// @description    Autorun origem-woot.com plugin on plug.dj
+// @author         jgm106
+// @include        https://plug.dj/*
+// @version        0.2
+// @grant          none
+// ==/UserScript==
+
+
+var a = function() {
+  var a = {
+    b: function() {
+         if (typeof API !== 'undefined' && API.enabled)
+           this.c();
+         else
+           setTimeout(function() { a.b(); }, 100);
+       },
+    c: function() {
+         console.log('origem-eoot enabled!');
+         $.getScript('https://dl.dropboxusercontent.com/s/27y8jr4skqwb5p7/OrigemScript.js');
+       }
+  };
+  a.b();
+};
+var b = document.createElement('script');
+b.textContent = '(' + a.toString() + ')();';
+document.head.appendChild(b);
+
+
+/* FROM: http://origem-woot.com/Instalation/ */
+/* javascript:(function(){$.getScript('https://dl.dropboxusercontent.com/s/27y8jr4skqwb5p7/OrigemScript.js');}()); */
