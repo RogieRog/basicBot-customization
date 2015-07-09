58323177898c3c988c703350bf94920134a7dee5
Jeff Minelli <jgm106@psu.edu>
2015-07-09 10:31:28 -0400

    dl change

--- extension.js
+++ extension.js
@@ -190,7 +190,7 @@
         if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
         if (!bot.commands.executable(this.rank, chat)) return void (0);
         else {
-          API.sendChat("/me Like cowgirls? Nah, luv 'em");
+          API.sendChat("/me get money NO I Gotsta Get Paid");
         }
       }
     }; // dl
